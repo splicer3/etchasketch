920dfb1b8c8293b702a4eda61fc0704eb45264a7
Matteo <matteodiio941@gmail.com>
2022-10-23 10:48:09 +0200

    Grid of divs now correctly renders. Still working on proper coloring.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-// This function creates ONE div for each call.
-function createDiv() {
+// This function creates ONE column for each call.
+function createColumn() {
+    let row = document.createElement('div');
+    row.className += ' row'
+    row.id = 'row'
+    return document.getElementById('sketchpad').append(row);
+}
+
+function createDivs() {
     let div = document.createElement('div');
-    div.style.width = '30px'
-    div.style.height = '30px'
-    div.style.backgroundColor = "blue"
-    return document.getElementById('sketchpad').append(div);      
+    div.className += ' pixels';
+    div.id = 'pixel';
+    return document.querySelector('#row').appendChild(div);
 }
 
 // This one gets the current slider value.
 function getValue() {
     let val = document.querySelector("#myRange");
     if (val.value != null)
-    return (val.value*val.value);
+    return (val.value);
     else
-    return (16*16);
+    return (16);
 }
 
 // This one generates as much divs as specified by the slider value.
@@ -21,7 +27,14 @@ function sketchGen() {
     document.getElementById('sketchpad').replaceChildren();
     let pixel = getValue();
     for (let i = 0; i < pixel; i++) {
-        createDiv ();
+        createColumn ();
+        for (let j = 0; j < pixel; j++) {
+            createDivs();
+        }
+        document.getElementById('row').id = 'rowed';
     }
 }
+$(".pixels").on("click", function () {
+    $(".pixels").addClass('clickedPixels');
+});
 
